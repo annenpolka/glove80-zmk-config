610891f28dbb11a35d964f3a8732b6a5d5f97675
Annenpolka <lancelbb@gmail.com>
2025-03-02 12:55:26 +0900

    Update artifact name from 'firmware' to 'glove80-zmk'

--- download_firmware.sh
+++ download_firmware.sh
@@ -13,8 +13,12 @@ API_URL="https://api.github.com/repos/$USER/$REPO/actions/artifacts"
 echo "最新のビルドアーティファクトを検索中..."
 ARTIFACTS=$(curl -s $API_URL)
 
+# アーティファクトの詳細確認
+echo "アーティファクト情報:"
+echo $ARTIFACTS | jq '.'
+
 # 最新のglove80.uf2アーティファクトを見つける
-ARTIFACT_ID=$(echo $ARTIFACTS | jq -r '.artifacts[] | select(.name == "firmware") | .id')
+ARTIFACT_ID=$(echo $ARTIFACTS | jq -r '.artifacts[] | select(.name == "glove80-zmk") | .id')
 
 if [ -z "$ARTIFACT_ID" ]; then
     echo "Error: 最新のファームウェアアーティファクトが見つかりません"
